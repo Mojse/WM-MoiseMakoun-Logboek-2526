f27bf7b63e34216aed4cbf3415619c06e354a2c3
bayoi <moisemakoun@gmail.com>
2025-10-10 00:39:35 +0200

    Zie logboek

---
+++ Taak1/webapp/js/script.js
@@ -0,0 +1,10 @@
+let btnConcerten = document.querySelector("#btnConcerten");
+
+let baseApiAddress = "https://makoun.be/LiveCountry/api/";
+
+function getApiConcerten(){
+
+}
+
+
+btnConcerten.addEventListener('click', getApiConcerten());
--- logboek.md
+++ logboek.md
@@ -1,3 +1,8 @@
 # Week 1
 - 4u les
 - zelfstudie W3schools
+
+# Week 2
+- 4u les
+- zelfstudie W3schools
+- Taak RESTful API => maken van html/css, js, php's
